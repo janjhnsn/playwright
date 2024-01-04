4494a428af60eeb89be57376eb96d575df58659f
Jan Johansen <jan.johansen@apply.agency>
2024-01-04 11:46:01 +0100

    Add Button tests

---
+++ components/Button/Button.test.js
@@ -0,0 +1,38 @@
+import { test, expect } from "@playwright/test";
+import { rootSelector as root } from "../../test/core";
+
+// Render function - do I get what is expected
+test("Button default renders correctly", async ({ page }) => {
+  await page.goto("iframe.html?viewMode=story&id=button--button-default");
+  await expect(page.locator(`${root} button`)).toBeVisible();
+});
+
+// Content function - does the value match what is expected
+test("Button has correct value", async ({ page }) => {
+  await page.goto("iframe.html?viewMode=story&id=button--button-value-x");
+  await expect(page.locator(`${root} button`)).toHaveText("X");
+});
+
+// Content function - does the value match what is expected
+test("Button has correct name attribute", async ({ page }) => {
+  await page.goto("iframe.html?viewMode=story&id=button--button-name-y");
+  await expect(page.locator(`${root} button`)).toHaveAttribute("name", "Y");
+});
+
+// Content function - does the value match what is expected
+test("Button has correct value and name attribute", async ({ page }) => {
+  await page.goto("iframe.html?viewMode=story&id=button--button-name-value-z");
+  await expect(page.locator(`${root} button`)).toHaveText("Z");
+  await expect(page.locator(`${root} button`)).toHaveAttribute("name", "Z");
+});
+
+// Functional function - does it alert when clicked
+test("Button when clicked has a dialog with correct output", async ({ page }) => {
+  await page.goto("iframe.html?viewMode=story&id=button--button-click-alert");
+  page.on("dialog", async (dialog) => {
+    expect(dialog.message()).toEqual('You clicked on me!')
+    await dialog.accept();
+  }
+  );
+  await page.locator(`${root} button`).click();
+});
---
+++ test/core.js
@@ -0,0 +1,1 @@
+export const rootSelector = "#storybook-root";
